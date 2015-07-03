69c0dabd483d2beb67e6d06324a046aee4655f42
Andrei Macavei <andrei.macavei89@gmail.com>
2015-07-03 14:00:50 +0300

    Temporary fix to make it possible to add use translated strings into app.
    It seems that the app deployment isn't copying the necessary .mo files into the installation path or I don't know which is that path.

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -4,13 +4,16 @@ import os, sys
 import locale
 import gettext
 
+import logging
+log = logging.getLogger()
+
 #  The translation files will be under
 #  @LOCALE_DIR@/@LANGUAGE@/LC_MESSAGES/@APP_NAME@.mo
 APP_NAME = "gnome-keysign"
 
-APP_DIR = os.path.join (sys.prefix, 'share')
-LOCALE_DIR = os.path.join(APP_DIR, 'i18n') # .mo files will then be located in APP_Dir/i18n/LANGUAGECODE/LC_MESSAGES/
-
+# APP_DIR = os.path.join (sys.prefix, 'share')
+# LOCALE_DIR = os.path.join(APP_DIR, 'i18n') # .mo files will then be located in APP_Dir/i18n/LANGUAGECODE/LC_MESSAGES/
+LOCALE_DIR = 'build/locale'
 # Now we need to choose the language. We will provide a list, and gettext
 # will use the first translation available in the list
 DEFAULT_LANGUAGES = os.environ.get('LANG', '').split(':')
@@ -33,4 +36,5 @@ gettext.textdomain(APP_NAME)
 
 gettext.bind_textdomain_codeset(APP_NAME, "UTF-8")
 
+log.info("The path to .mo files is %s", mo_location)
 language = gettext.translation(APP_NAME, mo_location, languages=languages, fallback=True)
