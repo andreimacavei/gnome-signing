aef766c0e0910aad88a5d4888b8bff0f91d206f4
Andrei Macavei <andrei.macavei89@gmail.com>
2015-08-04 12:44:28 +0300

    gpg: Fixed ImportError for Python3 on StringIO

--- keysign/gpg/gpg.py
+++ keysign/gpg/gpg.py
@@ -28,7 +28,10 @@ from distutils.spawn import find_executable
 import gpgme
 import gpgme.editutil
 from io import BytesIO
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 log = logging.getLogger()
