bff37ea8ac52dfc71e8b92a08f6ee79fc1567dbc
Andrei Macavei <andrei.macavei89@gmail.com>
2016-06-05 20:39:44 +0300

    gpg: Update lib function and remove validation from within it.
    The get_keylist lib function will now receive a `pair` argument.
    This is used to differentiate between public keys that are
    imported from other users/keyrings, and public keys that have
    a secret part existent inside that keyring.

--- keysign/gpg/gpg.py
+++ keysign/gpg/gpg.py
@@ -117,21 +117,28 @@ def import_keydata(gpgmeContext, keydata):
     return result
 
 
-def get_keylist(gpgmeContext, keyid=None, secret=False, expire_flag=False):
+def get_keylist(gpgmeContext, keyid=None, secret=False, pair=False):
     """Returns the keys found in @gpgmeContext
 
     @keyid: the unique id of a key.
     @secret: if set to True it returns the secret keys, else it returns the public keys.
-    @expire_flag: if set to True it also returns the expired keys.
+    @pair: if True it returns only the public keys that are associated with a secret key
     """
-    keys = []
-    for key in gpgmeContext.keylist(keyid, secret):
-        exp_date = datetime.fromtimestamp(float(key.subkeys[0].expires))
-        if expire_flag == False and key.subkeys[0].expires != 0 and exp_date.date() < datetime.today().date():
-            continue
-        keys.append(key)
+    secret_keys = [key for key in gpgmeContext.keylist(keyid, True)]
+    if secret:
+        return secret_keys
 
-    return keys
+    public_keys = [key for key in gpgmeContext.keylist(keyid, False)]
+    if pair:
+        pair_keys = []
+        for key in public_keys:
+            for skey in secret_keys:
+                if key.subkeys[0].fpr == skey.subkeys[0].fpr:
+                    pair_keys.append(key)
+                    break
+        return pair_keys
+
+    return public_keys
 
 def get_personal_keys(keyid=None, secret=False):
     """Returns the personal keys found in User's own keyring.
